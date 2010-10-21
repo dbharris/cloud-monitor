fa161587938aa24659de744f69ea3b61bdd97537
Drew Harris <dbharris@uvic.ca>
2010-10-21 15:16:27 -0700

    Formatting changes to make Matt happy.

--- monitor2.py
+++ monitor2.py
@@ -5,24 +5,23 @@ from web import httpserver
 
 render = web.template.render('templates/')
 server = xmlrpclib.ServerProxy("http://canfarpool.phys.uvic.ca:8111")
-urls = (
-	'/', 'index', 'error')
+urls = ('/', 'index', 'error')
 
 app = web.application(urls, globals())
 
 class index:
-	def GET(self):
-                try: 
-			cloudInfo = [server.get_cluster_resources(),server.get_highjobs(),server.get_newjobs(),server.get_schedjobs(),server.get_cluster_vm_resources()]
-			return render.index(cloudInfo)
+    def GET(self):
+        try: 
+	    cloudInfo = [server.get_cluster_resources(),server.get_highjobs(),server.get_newjobs(),server.get_schedjobs(),server.get_cluster_vm_resources()]
+	    return render.index(cloudInfo)
 		
-		except httplib.socket.error,e:
-			return render.error(e)	
+	except httplib.socket.error,e:
+	    return render.error(e)	
 
 
 
 if __name__ == "__main__": 
-	myfunc=app.wsgifunc()
-	web.httpserver.runsimple(myfunc,server_address=("142.104.60.42",8080))
+    myfunc=app.wsgifunc()
+    web.httpserver.runsimple(myfunc,server_address=("142.104.60.42",8080))
 	
 
